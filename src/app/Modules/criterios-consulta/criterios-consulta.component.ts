2218715807a225e2b44ffa1356c58e5f0f80a3f3
elkinjimenez <elkin.jiga@hotmail.com>
2020-07-27 10:33:39 -0500

    Creando criterios de consulta HTMl

---
+++ src/app/Modules/criterios-consulta/criterios-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criterios-consulta',
+  templateUrl: './criterios-consulta.component.html',
+  styleUrls: ['./criterios-consulta.component.css']
+})
+export class CriteriosConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/Modules/modules.module.ts
+++ src/app/Modules/modules.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TipoCuentaComponent } from './tipo-cuenta/tipo-cuenta.component';
+import { CriteriosConsultaComponent } from './criterios-consulta/criterios-consulta.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TipoCuentaComponent, CriteriosConsultaComponent],
   imports: [
     CommonModule,
     FormsModule
   ],
-  exports: []
+  exports: [
+    TipoCuentaComponent,
+  ]
 })
 export class ModulesModule { }
