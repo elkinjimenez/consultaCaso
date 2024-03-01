2d1123097006d8584b351e8fcaa8e100a79a5ac5
elkinjimenez <elkin.jiga@hotmail.com>
2024-03-01 17:11:24 -0500

    Terminando QR challenger.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { GenerateComponent } from './modules/generate/generate.component';
-import { GeneratedComponent } from './modules/generated/generated.component';
 
 export const routes: Routes = [
   { path: '', component: GenerateComponent },
--- src/app/modules/generate/generate.component.ts
+++ src/app/modules/generate/generate.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import QRCode from 'qrcode';
 
 @Component({
   selector: 'app-generate',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [FormsModule],
   templateUrl: './generate.component.html',
   styleUrl: './generate.component.css'
 })
 export class GenerateComponent {
+
+  qrData = '';
+  qrImage: string = '';
+
+  constructor() {
+    // this.generateQRCode();
+  }
+
+  generateQRCode() {
+    QRCode.toDataURL(this.qrData)
+      .then(url => {
+        this.qrImage = url;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
 
 }
